d979d21b0063bd25cd4748eb8c20fee89af3ee64
alex <deusten@gmail.com>
2020-12-18 02:17:00 +0200

    Fix

--- src/components/TrainingList.js
+++ src/components/TrainingList.js
@@ -4,7 +4,7 @@ import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-material.css';
 import Button from '@material-ui/core/Button';
-import Snackbar from '@material-ui/core/snackbar';
+import Snackbar from '@material-ui/core/Snackbar';
 import moment from 'moment/moment';
 
 
