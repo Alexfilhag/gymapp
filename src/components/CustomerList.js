d05f075afb0308295ef2f7f045b2389c00eaf107
alex <deusten@gmail.com>
2020-12-18 02:01:33 +0200

    Fix

--- src/components/CustomerList.js
+++ src/components/CustomerList.js
@@ -4,7 +4,7 @@ import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-material.css';
 import Button from '@material-ui/core/Button';
-import Snackbar from '@material-ui/core/snackbar';
+import Snackbar from '@material-ui/core/Snackbar';
 import AddCustomer from './AddCustomer';
 import EditCustomer from './EditCustomer';
 import AddTraining from './AddTraining';
